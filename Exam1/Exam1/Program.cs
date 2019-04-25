3346162e401193387d61f397b0862f15f2b9b696
Mobina Kashanian <Mobina.km@gmail.com>
2019-04-25 18:12:29 +0430

    mm

--- Exam1/Exam1/Program.cs
+++ Exam1/Exam1/Program.cs
@@ -9,7 +9,7 @@ namespace Exam1
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Q1Betweenness we = new Q1Betweenness("ff");
             we.Solve(4, new long[][] { new long[] {1,2 }, new long[] {4,1 }, new long[] {2,3 },new long[] {3,1 } });
         }
