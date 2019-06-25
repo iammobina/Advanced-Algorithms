cd868ed4fee1256a55a76f8973d4677c3b3533b9
Mobina Kashanian <Mobina.km@gmail.com>
2019-06-25 13:11:27 +0430

    A9

---
+++ A9/A9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A9
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //   Q1InferEnergyValues q1 = new Q1InferEnergyValues("xxx");
+         //   q1.Solve(2, new double[,]
+         //{{1 ,5 ,7},
+         //   { - 2, - 7, - 5},
+         //   //{0 ,0 ,1 ,0, 4 },
+         //   //{0 ,0 ,0 ,1 ,3 }
+         //   });
+         //   //  2
+
+//            Q2OptimalDiet diet = new Q2OptimalDiet("ddd");
+//            diet.Solve(3, 2, new double[,]
+//{ { - 1 ,- 1 },
+//{ 1 ,0},
+//{ 0 ,1},
+//{ -1 ,2,2},
+//{ -1 ,2} } );
+
+
+        }
+    }
+}
